5953684cd8949bf27023eff48f57c5d726b48013
Afnan Ali <afnan.alial537@gmail.com>
2025-10-05 21:34:43 +0300

    change app name & start onboarding

--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -1,4 +1,5 @@
 import 'package:chatting_app/core/routes/app_routes.dart';
+import 'package:chatting_app/features/onboarding/screen/onboarding_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:chatting_app/features/sign_in/screen/login.dart';
 
@@ -9,10 +10,14 @@ abstract class RoutesManager {
         {
           return CupertinoPageRoute(builder: (context) => LoginScreen());
         }
+      case AppRoutes.onboarding:{
+         return CupertinoPageRoute(builder: (context) => OnBoardingScreen());
+      }
       case AppRoutes.register:
         {
           // return CupertinoPageRoute(builder: (context)=> Register());
         }
     }
+    return null;
   }
 }
